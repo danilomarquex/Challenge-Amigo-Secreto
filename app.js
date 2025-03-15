b6aaa395939f4b97cb8655342e36fb72a24af2cd
danilomarquex <academico.danilo9@gmail.com>
2025-03-14 23:35:42 -0300

    Implementar uma função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+// Armazenar os nomes dos amigos
 let amigos = [];
+
+// função para adicionar um amigo
+function adicionarAmigo() {
+    const inputAmigos = document.getElementById('amigo');
+    const nomesDosAmigos = inputAmigos.value.trim();
+
+    // validar que o campo não está vazio
+    if (nomesDosAmigos === "") {
+        alert("Por favor, inserir um nome!");
+        return;
+    }
+
+    // validar que o nome não está duplicado
+    if (amigos.includes(nomesDosAmigos)) {
+        alert(`${nomesDosAmigos} já está na lista!`);
+        return;
+    }
+
+    // adicionar o nome na lista de amigos
+    amigos.push(nomesDosAmigos);
+
+    // Limpar campo de entrada
+    inputAmigos.value = "";
+
+}
+adicionarAmigo();
